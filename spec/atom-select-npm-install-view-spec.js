d6c16fd4b2754dd1ad491dbdde484aa9f830e9d3
mo <feng_zilong@163.com>
2016-06-11 01:21:41 +0800

    :tada:init

---
+++ spec/atom-select-npm-install-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomSelectNpmInstallView from '../lib/atom-select-npm-install-view';
+
+describe('AtomSelectNpmInstallView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
